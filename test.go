ba34b4bc37bc27d7241bea66a12d10e646675acd
Brandon S <bss233@nau.edu>
2021-03-01 05:12:25 -0700

    Mix and inverse aren't working

--- test.go
+++ test.go
@@ -10,50 +10,61 @@ func test(Rounds int) {
 	// Add round key
 	Chunks := chunkMessage(Message)
 
-	fmt.Printf("Emulating AES after %v rounds\n", Rounds)
-	CipherText := addKey(Chunks[0])
-	// loop
-	for i := 0; i < Rounds; i++ {
-		// SubBytes -- Working
+	CipherText := mixColumns(Chunks[0], 0)
+	fmt.Printf("Mixed columns : %v \n", CipherText)
+
+	PlainText := mixColumns(CipherText, 1)
+	fmt.Printf("Inverse mix : %v \n", PlainText)
+
+	/*
+		fmt.Printf("Emulating AES after %v rounds\n", Rounds)
+		CipherText := addKey(Chunks[0])
+		// loop
+		for i := 0; i < Rounds; i++ {
+			// SubBytes -- Working
+			CipherText = subBytes(CipherText, SBOX)
+			// ShiftRows -- Working
+			CipherText = shiftRows(CipherText, false)
+			// MixColumns -- Working?
+			CipherText = mixColumns(CipherText, 0)
+			// AddRoundKey
+			CipherText = addKey(CipherText)
+		}
+		// Final Round
+		// subBytes
 		CipherText = subBytes(CipherText, SBOX)
-		// ShiftRows -- Working
+		// shiftRows
 		CipherText = shiftRows(CipherText, false)
-		// MixColumns -- Working?
-		CipherText = mixColumns(CipherText, 0)
-		// AddRoundKey
+		// addKey
 		CipherText = addKey(CipherText)
-	}
-	// Final Round
-	// subBytes
-	CipherText = subBytes(CipherText, SBOX)
-	// shiftRows
-	CipherText = shiftRows(CipherText, false)
-	// addKey
-	CipherText = addKey(CipherText)
-	fmt.Printf("Cipher Text after AES : %v\n", CipherText)
+		fmt.Printf("Cipher Text after AES : %v\n", CipherText)
 
-	fmt.Printf("Emulating AES Decryption\n")
+		fmt.Printf("Emulating AES Decryption\n")
 
-	PlainText := addKey(CipherText)
-	// loop
-	for i := 0; i < Rounds; i++ {
+		PlainText := addKey(CipherText)
+
+		PlainText = shiftRows(PlainText, true)
+		// InvByte Sub
+		PlainText = subBytes(PlainText, RSBOX)
+		// loop
+		for i := 0; i < Rounds; i++ {
+			PlainText = shiftRows(PlainText, true)
+			// InvByte Sub
+			PlainText = subBytes(PlainText, RSBOX)
+			// Inv Mix
+			PlainText = mixColumns(PlainText, 1)
+			// Key addition
+			PlainText = addKey(PlainText)
+		}
+		// endloop
 		// Inv Shift Rows
 		PlainText = shiftRows(PlainText, true)
 		// InvByte Sub
 		PlainText = subBytes(PlainText, RSBOX)
-		// Inv Mix
-		PlainText = mixColumns(PlainText, 1)
 		// Key addition
 		PlainText = addKey(PlainText)
-	}
-	// endloop
-	// Inv Shift Rows
-	PlainText = shiftRows(PlainText, true)
-	// InvByte Sub
-	PlainText = subBytes(PlainText, RSBOX)
-	// Key addition
-	PlainText = addKey(PlainText)
 
-	fmt.Printf("Cipher Text after AES : %v\n", PlainText)
+		fmt.Printf("Cipher Text after AES : %v\n", PlainText)
+	*/
 
 }
